8a4e6958b37a2f732d02603313cf0465da50ce41
EGT890-YT <128317469+EGT890-YT@users.noreply.github.com>
2026-08-25 11:45:54 +0100

    Create tickle.js

---
+++ src/commands/Fun/tickle.js
@@ -0,0 +1,71 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { successEmbed, warningEmbed } from '../../utils/embeds.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("tickle")
+    .setDescription("Tickle another user!")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("The user you want to tickle.")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("message")
+        .setDescription("Customize the tickle message.")
+        .setRequired(false)
+    ),
+
+  category: 'Fun',
+
+  async execute(interaction, config, client) {
+    await InteractionHelper.safeDefer(interaction);
+
+    const tickler = interaction.user;
+    const target = interaction.options.getUser("user");
+    const customMessage = interaction.options.getString("message");
+
+    // Prevent tickling yourself
+    if (tickler.id === target.id) {
+      const embed = warningEmbed(
+        "🤭 Nice Try!",
+        `**${tickler.username}**, you can't tickle yourself!`
+      );
+
+      return await InteractionHelper.safeEditReply(interaction, {
+        embeds: [embed]
+      });
+    }
+
+    // Prevent tickling bots
+    if (target.bot) {
+      const embed = warningEmbed(
+        "🤖 Invalid Target",
+        "You can't tickle a bot! Find a real person to tickle."
+      );
+
+      return await InteractionHelper.safeEditReply(interaction, {
+        embeds: [embed]
+      });
+    }
+
+    const defaultMessage =
+      `😂 **${tickler.username}** is tickling **${target.username}**!`;
+
+    const description = customMessage
+      ? `😂 **${tickler.username}** tickles **${target.username}**!\n\n${customMessage}`
+      : defaultMessage;
+
+    const embed = successEmbed(
+      "🪶 Tickle Attack!",
+      description
+    );
+
+    await InteractionHelper.safeEditReply(interaction, {
+      embeds: [embed]
+    });
+  },
+};
