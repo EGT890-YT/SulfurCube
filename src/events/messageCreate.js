436cdadf7df5d8ff3729d87a759816f842cce847
EGT890-YT <128317469+EGT890-YT@users.noreply.github.com>
2026-09-13 21:49:46 +0100

    Fix counting game state import

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -14,6 +14,7 @@ import { createEmbed } from '../utils/embeds.js';
 import { isCommandEnabled } from '../services/commandAccessService.js';
 import {
   getCountingGameConfig,
+  saveCountingGameConfig,
   isValidCountingMessage,
   recordCorrectCount,
 } from '../services/countingGameService.js';
