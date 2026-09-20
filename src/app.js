e02055f51b0a8088549b0520b0ceb80adda86572
EGT890-YT <128317469+EGT890-YT@users.noreply.github.com>
2026-09-20 15:12:14 +0100

    Poll YouTube notifications every minute

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ import { getServerCounters, saveServerCounters, updateCounter } from './services
 import { logger, startupLog, shutdownLog } from './utils/logger.js';
 import { checkBirthdays } from './services/birthdayService.js';
 import { checkGiveaways } from './services/giveawayService.js';
+import { pollYouTubeNotifications } from './services/youtubeNotifyService.js';
 import { loadCommands, registerCommands as registerSlashCommands } from './handlers/loaders/commandLoader.js';
 import { runSafeTask, handleTaskError, ErrorCodes } from './utils/errorHandler.js';
 import { initializeMusic } from './services/music/riffySetup.js';
@@ -251,6 +252,7 @@ class TitanBot extends Client {
     cron.schedule('0 6 * * *', runSafeTask('birthday_check', () => checkBirthdays(this)));
     cron.schedule('* * * * *', runSafeTask('giveaway_check', () => checkGiveaways(this)));
     cron.schedule('*/15 * * * *', runSafeTask('counter_update', () => this.updateAllCounters()));
+    cron.schedule('* * * * *', runSafeTask('youtube_notifications', () => pollYouTubeNotifications(this)));
   }
 
   async updateAllCounters() {
